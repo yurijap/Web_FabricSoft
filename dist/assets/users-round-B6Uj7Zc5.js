62dd81344ff20689d0e0de2ec41e5baaa456fdbf
Juan Antonio Salazar Bolaños <135300313+JuanAntonioSalazarB@users.noreply.github.com>
2026-08-04 16:25:59 -0600

    Cambio 5

---
+++ dist/assets/users-round-B6Uj7Zc5.js
@@ -0,0 +1,11 @@
+import{d as e}from"./index-hDlw8vvS.js";/**
+ * @license lucide-react v1.17.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"m12 14 4-4",key:"9kzdfg"}],["path",{d:"M3.34 19a10 10 0 1 1 17.32 0",key:"19p75a"}]],s=e("gauge",o);/**
+ * @license lucide-react v1.17.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"M18 21a8 8 0 0 0-16 0",key:"3ypg7q"}],["circle",{cx:"10",cy:"8",r:"5",key:"o932ke"}],["path",{d:"M22 20c0-3.37-2-6.5-4-8a5 5 0 0 0-.45-8.3",key:"10s06x"}]],d=e("users-round",a);export{s as G,d as U};
