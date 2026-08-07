23d44151bbc8bca533aab32c7414da6378e1a4c0
Juan Antonio Salazar Bolaños <135300313+JuanAntonioSalazarB@users.noreply.github.com>
2026-08-07 12:43:34 -0600

    Cambios 7

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:4000/api';
+const baseURL = import.meta.env.VITE_API_URL || (import.meta.env.PROD ? '/api' : 'http://localhost:4000/api');
 
 const headersConfig: Record<string, string> = {
   'Content-Type': 'application/json',
