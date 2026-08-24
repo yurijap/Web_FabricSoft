ed34ce806b77cd8c15871615f990a4e38f693a3c
Juan Antonio Salazar Bolaños <135300313+JuanAntonioSalazarB@users.noreply.github.com>
2026-08-24 17:54:26 -0600

    Cambio 2 - 5:54PM

---
+++ dist/assets/check-sh7uTr1l.js
@@ -0,0 +1,6 @@
+import{c}from"./index-D_d0ys2f.js";/**
+ * @license lucide-react v1.17.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],t=c("check",e);export{t as C};
